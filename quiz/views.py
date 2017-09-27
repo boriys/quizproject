b416d7d4eebba35ac882210a891272564bf532b4
boriys <32261730+boriys@users.noreply.github.com>
2017-09-27 11:43:38 +0200

    Added 4 new views

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+def startpage(request):
+	return(request, "start.html")
+
+def quiz(request, quiz_number):
+	return render(request, "quiz.html")
+
+def question(request, quiz_number, question_number):
+	return render (request, "question.html")
+
+def completed (request, quiz_number):
+	return render (request, "result.html")
+
 # Create your views here.
